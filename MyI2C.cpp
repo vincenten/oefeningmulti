e72f945d5ba2492443124c66da9c740bc6eab621
Niels Beyen <r0349893@student.vives.be>
2014-11-21 09:55:33 +0100

    test

--- MyI2C.cpp
+++ MyI2C.cpp
@@ -1,20 +1,40 @@
-#include "MyI2C.h"
+#include "I2C.h"
+#include <string.h>
 #include <linux/i2c-dev.h>
 #include <sys/ioctl.h>
 #include <fcntl.h>          //O_RDWR
 #include <stdio.h>          //perror
 
-namespace TRexLib{
+namespace RemoteMemoryLib{
     
-    /*
-     * Pointer to write function used for i2c communication.
-     * Also see http://linux.die.net/man/2/write
-     */
     ssize_t (* pi_i2c_write)(int, const void *, size_t) = write;
-    
-    /*
-     * Pointer to read function used for i2c communication.
-     * Also see http://linux.die.net/man/2/read
-     */
     ssize_t (* pi_i2c_read)(int, void *, size_t) = read;
+    
+    I2C::I2C(const char * device, int i2caddress)
+    {
+	strcpy(this->device,device);
+	openBus();
+	acquireBus(i2caddress);
+    }
+
+    int I2C::openBus(void)
+    {
+	this->handle = open(this->device, O_RDWR);
+	return handle;
+    }
+
+    int I2C::acquireBus(int i2caddress)
+    {
+	return ioctl(this->handle,I2C_SLAVE,i2caddress);
+    }
+
+    int I2C::write(int address, char * data, int length)
+    {
+	return pi_i2c_write(this->handle,data,length);
+    }
+
+    int I2C::read(int address, char * data, int length)
+    {
+	return pi_i2c_read(this->handle,data,length);
+    }
 }
--- MyI2C.h
+++ MyI2C.h
@@ -1,58 +1,25 @@
-#include "I2C.h"
 
-#ifndef MY_I2C_HEADER
-#define MY_I2C_HEADER
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-namespace TRexLib{
+#ifndef I2C_HEADER
+#define I2C_HEADER
 
-    class MyI2C: public I2C{
+namespace RemoteMemoryLib{
+
+    class I2C{
         public:
-            /*
-             * Constructor
-             *
-             * @device the device path of the I2C bus (for example /dev/i2c-1)
-             * @i2caddress the I2C slave device address
-             */
-            MyI2C(const char * device, int i2caddress);
+            I2C(const char * device, int i2caddress);
+			int write(int address, char * data, int length);
+			int read(int address, char * data, int length);
 
-            /*
-             * Write data to an I2C slave device.
-             *
-             * @address the I2C slave device address (not actually used but kept for compatibility with mbed functions)
-             * @data buffer containing the bytes that should be written to the slave
-             * @length number of bytes in the buffer to write
-             *
-             * @return 0 on failure
-             */
-		int write(int address, char * data, int length);
-
-            /*
-             * Read data from an I2C slave device.
-             *
-             * @address the I2C slave device address (not actually used but kept for compatibility with mbed functions)
-             * @data buffer to store the byte values received from the device
-             * @length number of bytes to read from the device (should be at least size of data buffer)
-             *
-             * @return 0 on failure
-             */
-		int read(int address, char * data, int length);
-
-        protected:
-            /*
-             * Open the I2C bus device so the read and write functions can be used.
-             *
-             * @return a non-negative integer if the open action was succesful
-             */
+        private:
             int openBus(void);
-
-            /*
-             * Set the slave device address and thereby acquire the bus.
-             *
-             * @i2caddress the I2C slave device address
-             *
-             * @return a non-negative integer if the aquire action was succesful
-             */
             int acquireBus(int i2caddress);
+            char device[128];
+            int handle;
     };
 }
 
