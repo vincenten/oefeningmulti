00d618b277fca53c70bddb2c0c9d27074328c7e5
Niels Beyen <r0349893@student.vives.be>
2014-11-21 11:01:42 +0100

    i2c overerving

--- MyI2C.cpp
+++ MyI2C.cpp
@@ -1,4 +1,4 @@
-#include "I2C.h"
+#include "MyI2C.h"
 #include <string.h>
 #include <linux/i2c-dev.h>
 #include <sys/ioctl.h>
@@ -10,30 +10,30 @@ namespace RemoteMemoryLib{
     ssize_t (* pi_i2c_write)(int, const void *, size_t) = write;
     ssize_t (* pi_i2c_read)(int, void *, size_t) = read;
     
-    I2C::I2C(const char * device, int i2caddress)
+    MyI2C::MyI2C(const char * device, int i2caddress):I2C(device, i2caddress)
     {
 	strcpy(this->device,device);
 	openBus();
 	acquireBus(i2caddress);
     }
 
-    int I2C::openBus(void)
+    int MyI2C::openBus(void)
     {
 	this->handle = open(this->device, O_RDWR);
 	return handle;
     }
 
-    int I2C::acquireBus(int i2caddress)
+    int MyI2C::acquireBus(int i2caddress)
     {
 	return ioctl(this->handle,I2C_SLAVE,i2caddress);
     }
 
-    int I2C::write(int address, char * data, int length)
+    int MyI2C::write(int address, char * data, int length)
     {
 	return pi_i2c_write(this->handle,data,length);
     }
 
-    int I2C::read(int address, char * data, int length)
+    int MyI2C::read(int address, char * data, int length)
     {
 	return pi_i2c_read(this->handle,data,length);
     }
